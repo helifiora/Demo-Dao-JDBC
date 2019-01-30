b477e773eb8037649a3554a8cb9aea050618e546
Helielton Firoramonte <helifiora@gmail.com>
2019-01-30 14:40:01 -0200

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,5 +28,12 @@ public class Program {
         System.out.println("=== TEST 3: seller findAll ===");
         List<Seller> list2 = sellerDao.findAll();
         list2.forEach(System.out::println);
+
+
+        System.out.println();
+        System.out.println("=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! new id: " + newSeller.getId());
     }
 }
